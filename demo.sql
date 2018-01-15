ea76a810ce027ab308c3ead2b99420473bf26fa2
aru2 <rod.b.doe@nordstrom.com>
2018-01-15 11:18:31 -0800

    Initial submission.

---
+++ demo.sql
@@ -0,0 +1,137 @@
+--create staged data table
+create table s_music(
+	id integer not null primary key autoincrement,
+	composer text not null,
+	title text not null,
+	duration text not null
+);
+
+--create dimension table
+create table d_music(
+	dim_music_id integer not null primary key autoincrement,
+	composer text not null,
+	title text not null,
+	duration text not null,
+    created text not null,
+	modified text
+);
+
+--insert some rows into stage data.
+insert into s_music (composer, title, duration)
+values
+('Tchaikovsky', 'Nocturne in F', '5:31'),
+('J.S. Bach', 'Air on a G String', '5:10'),
+('Chopin', 'Ballade No. 1', '8:19'),
+('Pachelbel', 'Canon in D', '5:10'),
+('Debussy', 'Claire de Lune', '4:46'),
+('Janacek', 'Idyl for Strings', '5:38'),
+('Beethoven', 'Moonlight Sonata', '6:38'),
+('Mendelssohn', 'Songs Without Words', '2:54'),
+('Tchaikovsky', 'Swan Lake (excerpt)', '7:11'),
+('Beethoven', 'Pathetique Sonata', '5:16'),
+('Prokofiev', 'Romeo and Juliet', '3:42'),
+('Saint-Saens', 'Aquarium', '2:13'),
+('The Cranberries', 'Linger', '4:35');
+
+--Discover new records in staged data table that are NOT 
+--in the dimension table.
+insert into d_music (composer, title, duration)
+select composer, title, duration from s_music
+except
+select 
+	d.composer,
+	d.title,
+	d.duration
+	from d_music d
+	inner join s_music s on 
+	s.composer = d.composer and
+	s.title = d.title and
+	s.duration = d. duration
+	where d.modified is null;
+
+--Update a row in staged data.	
+update s_music set duration = '5:12' where title = 'Air on a G String';
+
+--Discover rows that must be modified in the dimension table.
+select composer, title, duration from d_music
+except
+select
+	d.composer,
+	d.title,
+	d.duration
+	from s_music s
+	inner join d_music d on 
+	s.composer = d.composer and
+	s.title = d.title and
+	s.duration = d. duration
+	where d.modified is null;
+
+--do update from select
+UPDATE   books
+SET books.primary_author = authors.name
+FROM   books
+INNER JOIN authors ON   books.author_id = authors.id
+WHERE books.title = 'The Hobbit'
+
+
+update d_music
+set modified = date('now')
+from d_music 
+select composer, title, duration
+except
+select
+	d.composer,
+	d.title,
+	d.duration
+	from s_music s
+	inner join d_music d on 
+	s.composer = d.composer and
+	s.title = d.title and
+	s.duration = d. duration
+	where d.modified is null;
+
+--Update a row in the dimension data.
+update d_music set modified = date('now') where
+composer = 'J.S. Bach' and  
+title = 'Air on a G String' and 
+duration = '5:10';
+
+--Insert the new row in the dimension table.
+insert into d_music (composer, title, duration)
+values
+('J.S. Bach', 'Air on a G String', '5:12');
+
+
+	
+update s_music set duration = '5:12' where title = 'Air on a G String'
+	
+--Discover rows that must be added to the dimension table.
+insert into d_music (composer, title, duration)
+values
+select composer, title, duration from s_music
+except
+select 
+	d.composer,
+	d.title,
+	d.duration
+	from d_music d
+	inner join s_music s on 
+	s.composer = d.composer and
+	s.title = d.title and
+	s.duration = d. duration
+	where d.modified is null;
+	
+--Add a row in the dimension table data.
+insert into d_music (composer, title, duration)
+values
+('J.S. Bach', 'Air on a G String', '5:12');
+
+
+--Discover rows that have been deleted from staging.
+select composer, title, duration from d_music
+where modified is null
+except
+select composer, title, duration from s_music s;
+
+
+ 
---
+++ handle_changed_content.py
@@ -0,0 +1,135 @@
+import datetime
+import sys
+import sqlite3
+
+def run(event, context):
+    start = datetime.datetime.now()
+    process_date = str(datetime.date.today())
+
+    # Discovers rows that have been deleted from staging and as such
+    # must be marked as deactivated in the dimension.
+    select_dim_rows_to_deactivate = '''
+    select composer, title, duration from d_music where modified is null
+    except
+    select composer, title, duration from s_music
+    '''
+
+    # Discovers rows that must be modified in the dimension.
+    select_dim_rows_to_modify_sql = '''
+    select composer, title, duration from d_music where modified is null
+    except
+    select
+        d.composer,
+        d.title,
+        d.duration
+    from s_music s
+    inner join d_music d on
+    s.composer = d.composer and
+    s.title = d.title and
+    s.duration = d. duration
+    where d.modified is null;
+    '''
+
+    # Discover rows that must be added to the dimension.
+    select_dim_rows_to_add_sql = '''
+    select
+        composer,
+        title,
+        duration
+    from s_music
+    except
+    select
+    d.composer,
+    d.title,
+    d.duration
+    from d_music d
+    inner join s_music s on
+    s.composer = d.composer and
+    s.title = d.title and
+    s.duration = d.duration
+    where d.modified is null;
+    '''
+
+    modify_sql = '''
+    update d_music set modified=date('now')
+    where composer = ? and title = ? and duration = ? and modified is null
+    '''
+
+    insert_sql = '''
+    insert into d_music (composer, title, duration, created) values (?, ?, ?, ?)
+    '''
+    database_file = 'music.sqlite3'
+    conn = None
+    cur = None
+
+    try:
+        conn = sqlite3.connect(database_file)
+    except:
+        print('Cannot open connection to %s' % database_file)
+        for msg in sys.exc_info():
+            print(msg)
+
+    try:
+        cur = conn.cursor()
+    except:
+        print('Cannot open cursor to execute query.')
+        for msg in sys.exc_info():
+            print(msg)
+
+    composer = None
+    title = None
+    duration = None
+
+    count_changed_rows = 0
+    count_new_rows = 0
+
+    try:
+        # Query rows in dimension that should get a modified date.
+        cur.execute(select_dim_rows_to_modify_sql)
+    except:
+        print('Cannot execute selection of dimension rows to modify.')
+        for msg in sys.exc_info():
+            print(msg)
+
+    changed_rows = cur.fetchall()
+    count_changed_rows = len(changed_rows)
+    if count_changed_rows > 0:
+        for row in changed_rows:
+            composer, title, duration = row
+            cur.execute(modify_sql, (composer, title, duration))
+
+        conn.commit()
+
+    try:
+        # Query rows to add to dimension from staging.
+        cur.execute(select_dim_rows_to_add_sql)
+    except:
+        print('Cannot execute selection of dimension rows to add.')
+        for msg in sys.exc_info():
+            print(msg)
+
+    new_rows = cur.fetchall()
+    count_new_rows = len(new_rows)
+    
+    if count_new_rows > 0:
+        for row in new_rows:
+            composer, title, duration = row
+            cur.execute(insert_sql, (composer, title, duration, process_date))
+
+        conn.commit()
+
+    cur.close()
+    conn.close()
+
+    end = datetime.datetime.now()
+    duration_ms = int(1000 * (end - start).total_seconds())
+    print('Processed %d changed rows and %d new rows in %d milliseconds.' % (count_changed_rows, count_new_rows, duration_ms))
+
+if __name__ == '__main__':
+    run(event={}, context={})
+
+
+
+
+
+
